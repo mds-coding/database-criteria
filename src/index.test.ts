b3c4834da5ed81fd6d02e83d090822ea8086fd67
mdos-san <mdossan@protonmail.com>
2024-11-07 04:51:17 +0100

    feat: improve typing by adding defaults

--- src/index.test.ts
+++ src/index.test.ts
@@ -8,6 +8,6 @@ type Cat = {
 test("`DatabaseCriteria` can be created", () => {
   const criteria = new DatabaseCriteria<Cat, 'name'>('name', "EQ", "Plume");
   expect(criteria).toBeDefined();
-  const criteria2 = new DatabaseCriteria<Cat, 'age'>('age', "GTE", 15);
+  const criteria2 = new DatabaseCriteria<Cat, 'age'>('age', "GTE", 4);
   expect(criteria2).toBeDefined();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 type Comparator = "EQ" | "LT" | "LTE" | "GT" | "GTE";
 
-export class DatabaseCriteria<T, K extends keyof T, V = T[K]>   {
-  constructor(public field: K, public comparator: Comparator, public value: V) {}
+export class DatabaseCriteria<T, K extends keyof T = keyof T, V = T[K]>   {
+  public constructor(public field: K, public comparator: Comparator, public value: V) {}
 } 
